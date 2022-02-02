1ef135d2af27aa9f00dbc6eded159ef399466f5f
rojon <rjrojon@gmail.com>
2022-02-02 11:10:24 +0600

    exclude unique

--- models/Payout.js
+++ models/Payout.js
@@ -1,71 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const Payouts = new Schema(
-    {
+  {
+    name: {
+      type: String,
+      default: "",
+    },
+    userID: {
+      type: String,
+      required: true,
+    },
+    purchaseType: {
+      type: String,
+      enum: ["SubCategory", "Test Series"],
+      required: true,
+    },
+    email: {
+      type: String,
+      default: "",
+    },
+    contact: {
+      type: String,
+      default: "",
+    },
+    orderID: {
+      type: String,
+      default: "",
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    subCategoryID: {
+      type: String,
+      default: "",
+    },
+    testID: {
+      type: String,
+      default: "",
+    },
+    status: {
+      type: String,
+      enum: ["Success", "Pending", "Failed"],
+      default: "Pending",
+    },
+  },
+  { timestamps: true }
+);
 
-       name:
-       {
-           type: String,
-           default: ''
-       },
-       userID:
-       {
-           type: String,
-           required: true
-       },
-       purchaseType:
-       {
-           type: String,
-           enum: ['SubCategory', 'Test Series'],
-           required: true
-       },
-       email:
-
-       {
-           type: String,
-           default: ''
-       },
-       contact:
-       {
-           type: String,
-           default: ''
-       },
-       orderID:
-       {
-           type: String,
-           default: ''
-       },
-       amount:
-       {
-           type: Number,
-           required: true
-           
-
-       },
-       subCategoryID:
-       {
-           type: String,
-           default: ''
-       },
-       testID:
-       {
-           type: String,
-           default: ''
-       },
-       status:
-       {
-           type: String,
-           enum: ['Success', 'Pending', 'Failed'],
-           default: 'Pending'
-       }
-
-      
-
-    },{timestamps: true}
-)
-
-const Payout = mongoose.model("Payout", Payouts );
+const Payout = mongoose.model("Payout", Payouts);
 
 exports.Payout = Payout;
--- models/Section.js
+++ models/Section.js
@@ -3,27 +3,26 @@ const UserSectionRes = require("./UserSectionRes");
 const { Schema, model } = mongoose;
 
 const sectionSchema = new Schema({
-	sectionName: {
-		type: String,
-		required: true,
-	},
-	questions: [
-		{
-			type: Schema.Types.ObjectId,
-			required: true,
-			ref: "Question",
-			unique: true,
-		},
-	],
+  sectionName: {
+    type: String,
+    required: true,
+  },
+  questions: [
+    {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Question",
+    },
+  ],
 });
 
 sectionSchema.methods.getUserRes = async function (userID) {
-	const section = this;
-	const sectionRes = await UserSectionRes.findOne({
-		userID,
-		sectionID: section._id,
-	});
-	return sectionRes;
+  const section = this;
+  const sectionRes = await UserSectionRes.findOne({
+    userID,
+    sectionID: section._id,
+  });
+  return sectionRes;
 };
 
 const Section = model("Section", sectionSchema);
